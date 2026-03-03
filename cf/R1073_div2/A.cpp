e2d5c460d0e2d2f98b47deffdbcbdbc12a660c7b
Natural-Horse <1648848391@qq.com>
2026-03-03 19:54:58 +0800

    old computer with gitignore

---
+++ cf/R1073_div2/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int, int>
+#define mkp make_pair
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    vector<pii> a(n + 1, 0);
+    vector<bool> b1(n + 1, false), b2(n + 1, false);
+    for (int i = 1; i <= n; i++){
+        cin >> a[i];
+    }
+    
+}
+
+int main(){
+
+}
